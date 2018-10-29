7bb75bf34420527569131292c5ef958f96cf8393
Steven Meads <sbmeads@gmail.com>
2018-10-28 21:36:51 -0600

    add additioanl loaders and index.scss file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-(function () {
-  console.log("Here we go...");
-}());
+import React from "react";
+import ReactDOM from "react-dom";
+
+let HereWeGo = () => {
+  return <h1>Here we go...</h1>
+}
+
+ReactDOM.render(
+  <HereWeGo/>,
+  document.getElementById("root")
+);
