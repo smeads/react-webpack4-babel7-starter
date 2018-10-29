0af0eb963de166d894b54d106f8b1597e6ec77d2
Steven Meads <sbmeads@gmail.com>
2018-10-28 22:38:01 -0600

    move back to yarn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// this line is new
-// we now have some nice styles on our react app
 import "index.scss";
+
 let HelloWorld = () => {
   return <h1>Hello there World!</h1>
 }
