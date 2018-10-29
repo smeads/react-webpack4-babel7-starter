e9f70b7c9eab0f5e6c38d4fc4e73a50adc043775
Steven Meads <sbmeads@gmail.com>
2018-10-28 22:16:07 -0600

    Update webpack config babelrc etc

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-let HereWeGo = () => {
-  return <h1>Here we go...</h1>
+// this line is new
+// we now have some nice styles on our react app
+import "index.scss";
+let HelloWorld = () => {
+  return <h1>Hello there World!</h1>
 }
-
 ReactDOM.render(
-  <HereWeGo/>,
+  <HelloWorld/>,
   document.getElementById("root")
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,43 @@
-const path = require('path');
+import path from 'path';
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 module.exports = {
-  // these are used to tell our server what has to be compiled and from where
-  // It also tells where to put the outputted compiled version
   entry: path.join(__dirname,'src','index.js'),
   output: {
     path: path.join(__dirname,'build'),
     filename: 'index.bundle.js'
   },
   mode: process.env.NODE_ENV || 'development',
-  // this is used so that we can import anything from src folder in relative paths instead of absolute ones.
   resolve: {
     modules: [path.resolve(__dirname, 'src'), 'node_modules']
   },
-  // this tells the webpack-dev-server what files are needed to be served. 
   devServer: {
     contentBase: path.join(__dirname,'src')
+  },
+  module: {
+    rules: [
+      {
+        // this is so that we can compile any React,
+        // ES6 and above into normal ES5 syntax
+        test: /\.(js|jsx)$/,
+        // we do not want anything from node_modules to be compiled
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        loaders: ['file-loader']
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
